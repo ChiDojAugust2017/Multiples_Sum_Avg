5df2fe145a851bfc7b05cdeacc1448dc59b1f526
Scott Tillman <Scotttillman23@gmail.com>
2017-07-30 11:18:05 -0500

    avg

---
+++ Multiples_Sum_Average.py
@@ -0,0 +1,17 @@
+#for i in range(1, 1001, 2):
+  #  print(i)
+
+#for num in range(5, 1000005, 5):
+    #print(num)
+
+a = [1, 2, 5, 10, 255, 3]
+print (sum(a))
+print "is:", sum(a)/6
+
+
+p = [20, 21, 13, 25, 16, 27, 32, 18]
+
+print(sum(p))
+print "is:", sum(p)/8
+
+
